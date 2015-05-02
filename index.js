791e6795ae517d9ecac5467fdc40d40907d8768c
vasupalanisamy <vasu.palanisamy@gmail.com>
2015-05-02 02:30:46 -0700

    updated remaining APIs

--- index.js
+++ index.js
@@ -5,6 +5,11 @@ let morgan = require('morgan')
 let nodeify = require('bluebird-nodeify')
 let mime = require('mime-types')
 require('songbird')
+// let bluebird = require('bluebird')
+let rimraf = require('rimraf')
+let mkdirp = require('mkdirp')
+// bluebird.longStackTraces()
+require('longjohn')
 
 const NODE_ENV = process.env.NODE_ENV
 const PORT = process.env.PORT || 8000
@@ -16,42 +21,83 @@ if (NODE_ENV === 'development') {
 	app.use(morgan('dev'))
 }
 app.listen(PORT, ()=> console.log(`LISTENING @ http://127.0.0.1:${PORT}`))
-app.get('*', setFilePath, sendHeaders, (req, res) => {
+app.get('*', setFileMeta, sendHeaders, (req, res) => {
 	if(res.body) {
 		res.json(res.body)
 		return
 	}
 	fs.createReadStream(req.filePath).pipe(res)
 })
-app.head('*', setFilePath, sendHeaders, (req, res) => res.end())
-app.delete('*', setFilePath, (req, res, next) => {
+app.head('*', setFileMeta, sendHeaders, (req, res) => res.end())
+app.delete('*', setFileMeta, (req, res, next) => {
 	async() => {
-		let stat = await fs.promise.stat(filePath)
-	}
+		if(!req.stat) return res.send(400, 'Invalid Path')
+		if(req.stat.isDirectory()) {
+			await rimraf.promise(req.filePath)
+		} else {
+			await fs.promise.unlink(req.filePath)
+		}
+		res.end()
+	}().catch(next)
 })
 
-function setFilePath (req, res, next) {
+app.put('*', setFileMeta, setDirDetails, (req, res, next) => {
+	async ()=> {
+		if(req.stat) return res.send(405, 'File exists')
+		await mkdirp.promise(req.dirPath)
+		if(!req.isDir) req.pipe(fs.createWriteStream(req.filePath))
+		res.end()
+	}().catch(next)
+})
+
+app.post('*', setFileMeta, setDirDetails, (req, res, next) => {
+	async ()=> {
+		if(!req.stat) return res.send(405, 'File does not exist')
+		if(req.isDir) return res.send(405, 'Path is a directory')
+		await fs.promise.truncate(req.filePath, 0)
+		req.pipe(fs.createWriteStream(req.filePath))
+		res.end()
+	}().catch(next)
+})
+
+
+function setDirDetails(req, res, next) {
+	let endWithSlash = req.filePath.charAt(req.filePath.length-1) === path.sep
+	console.log('endWithSlash' + endWithSlash)
+	console.log('req.filePath.charAt(req.filePath-1)' + req.filePath.charAt(req.filePath-1))
+	console.log('path.sep' + path.sep)
+	let hasExt = path.extname(req.filePath) !== ''
+	console.log('hasExt' + hasExt)
+	req.isDir = endWithSlash || !hasExt
+	console.log('req.isDir' + req.isDir)
+	req.dirPath = req.isDir ? req.filePath : path.dirname(req.filePath)
+	next()
+}
+
+function setFileMeta (req, res, next) {
 	req.filePath = path.resolve(path.join(ROOT_DIR, req.url))
 	if(req.filePath.indexOf(ROOT_DIR) !== 0) {
 		res.send(400, 'Invalid path')
 		return
 	}
-	next()
+	fs.promise.stat(req.filePath)
+		.then(stat => req.stat = stat, () => req.stat = null)
+		.nodeify(next)
 }
 
 function sendHeaders(req, res, next) {
 	nodeify(async ()=> {
-		let filePath = req.filePath
-		let stat = await fs.promise.stat(filePath)
-		if(stat.isDirectory()){
-			let files = await fs.promise.readdir(filePath)
-			res.body = JSON.stringify(files)
-			res.setHeader('Content-Length', res.body.length)
-			res.setHeader('Content-Tyoe', 'application/json')
-			return
+		if(req.stat){
+			if(req.stat.isDirectory()){
+				let files = await fs.promise.readdir(req.filePath)
+				res.body = JSON.stringify(files)
+				res.setHeader('Content-Length', res.body.length)
+				res.setHeader('Content-Tyoe', 'application/json')
+				return
+			}
+			res.setHeader('Content-Length', req.stat.size)
+			let contentType = mime.contentType(path.extname(req.filePath))
+			res.setHeader('Content-Tyoe', contentType)
 		}
-		res.setHeader('Content-Length', stat.size)
-		let contentType = mime.contentType(path.extname(filePath))
-		res.setHeader('Content-Tyoe', contentType)
 	}(), next)
 }
