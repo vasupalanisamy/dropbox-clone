2f36faa9d1468beb08aa1f149277503eba1740f6
vasupalanisamy <vasu.palanisamy@gmail.com>
2015-05-03 03:29:13 -0700

    TCP support updated

--- client/client.js
+++ client/client.js
@@ -1,8 +1,9 @@
 let jot = require('json-over-tcp')
 const SERVER_TCP_PORT = process.env.SERVER_TCP_PORT || 8001
+const CLIENT_ID = process.env.CLIENT_ID || 10001
 function createConnection(){
   let socket = jot.connect(SERVER_TCP_PORT, function(){
-    socket.write({clientId: "10001"})
+    socket.write({clientId: CLIENT_ID})
   })
 
   socket.on('data', function(data){
